baaab4bddc98a45152df5ef062e3c132ed11c57e
Sosnowsky <juan.manuel.losada.sosnovsky@gmail.com>
2023-06-30 10:49:44 +0200

    Update version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url="https://github.com/uit-cosmo/filtered-point-process",
     download_url="https://github.com/uit-cosmo/3ppy/archive/refs/tags/1.1.tar.gz",
     license="MiT",
-    version="1.2",
+    version="1.3",
     packages=["superposedpulses"],
     python_requires=">=3.0",
     install_requires=[
