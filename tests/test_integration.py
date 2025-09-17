4095d2dfce4303452b3f1d69801921af3a1d63b8
Sosnowsky <jlo044@uit.no>
2025-09-17 11:47:46 +0200

    Fix test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -11,25 +11,28 @@ def test_moments_of_realization():
     Equation 23 is used for the root mean square value of the realization.
 
     """
-    waiting_time = 100
-    intermitency = 1 / waiting_time
+    waiting_time = 3.7
+    intermittency = 1 / waiting_time
     average_amplitude = 1
     average_squared_amplitude = 2
     integral_exponential_pulse_shape = 1
     integral_square_exponential_pulse_shape = 0.5
 
-    model = sp.PointModel(waiting_time=waiting_time, total_duration=100_000, dt=0.01)
+    model = sp.PointModel(waiting_time=waiting_time, total_duration=100_000, dt=0.1)
     times, signal = model.make_realization()
 
-    mean_theory = intermitency * average_amplitude * integral_exponential_pulse_shape
+    mean_theory = intermittency * average_amplitude * integral_exponential_pulse_shape
     rms_squared_theory = (
-        intermitency
+        intermittency
         * average_squared_amplitude
         * integral_square_exponential_pulse_shape
     )
 
     mean_realization = np.mean(signal)
-    rms_squared_realization = np.mean(signal**2)
+    rms_squared_realization = np.mean((signal - mean_realization)**2)
 
-    np.testing.assert_allclose(mean_realization, mean_theory, rtol=0.10)
-    np.testing.assert_allclose(rms_squared_realization, rms_squared_theory, rtol=0.20)
+    print(mean_realization, mean_theory)
+    print(rms_squared_realization, rms_squared_theory)
+
+    np.testing.assert_allclose(mean_realization, mean_theory, rtol=0.05)
+    np.testing.assert_allclose(rms_squared_realization, rms_squared_theory, rtol=0.05)
